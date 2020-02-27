395aa331227326738a0f8fc97b0773c82f815367
LE CALVEZ Esteban <lecalvez.esteban@gmail.com>
2020-02-27 16:49:27 +0100

    Icon + Detail + generate image

--- IosImgFlip/IosImgFlip/GenerateMemeViewController.swift
+++ IosImgFlip/IosImgFlip/GenerateMemeViewController.swift
@@ -17,64 +17,54 @@ class GenerateMemeViewController: UIViewController{
     
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var name: UILabel!
-    
-
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var validateButton: UIButton!
+    @IBOutlet weak var foregroundImageLoader: UIView!
     
     override func viewDidLoad() {
-//        let postData = NSMutableData(data: "UserID=351".data(using: String.Encoding.utf8)!)
-//        let urlRequest = NSMutableURLRequest(url: NSURL(string: "https://api.imgflip.com/caption_image")! as URL,
-//                                          cachePolicy: .useProtocolCachePolicy,
-//                                          timeoutInterval: 10.0)
-//        let params = ["username":"iosapi2020",
-//        "password":"iosapi2020",
-//        "template_id":myData!["id"].stringValue,
-//        "max_font_size":"20",
-//        "boxes[0][text]":"David",
-//        "boxes[1][text]":"Tofigh",
-//        ]
-//        let jsonString = params.reduce("") { "\($0)\($1.0)=\($1.1)&" }
-//        let jsonData = jsonString.data(using: .utf8, allowLossyConversion: false)!
-//        urlRequest.addValue("application/x-www-form-urlencoded", forHTTPHeaderField:"Content-Type")
-//        urlRequest.httpBody  = jsonData
-//
-        
-        
-        
+        image.kf.setImage(with: URL(string:myData!["url"].stringValue))
+        name.text = myData!["name"].stringValue
+    }
+    @IBAction func onButtonPressed(_ sender: Any) {
+
+        changeImage()
+    }
+    
+    func changeImage(){
         let url = URL(string: "https://api.imgflip.com/caption_image")!
         var request = URLRequest(url: url)
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
-       let params = [
+        let text1 = self.firstText.text
+        let text2 = self.secondText.text
+        let params = [
             "username":"iosapi2020",
-           "password":"iosapi2020",
-           "template_id":myData!["id"].stringValue,
-           "boxes[0][text]":"David",
-           "boxes[1][text]":"Tofigh",
-       ]
-       let jsonString = params.reduce("") { "\($0)\($1.0)=\($1.1)&" }
-       let jsonData = jsonString.data(using: .utf8, allowLossyConversion: false)!
-       request.httpBody  = jsonData
+            "password":"iosapi2020",
+            "template_id":myData!["id"].stringValue,
+            "boxes[0][text]":text1!,
+            "boxes[1][text]":text2!,
+        ]
+        let jsonString = params.reduce("") { "\($0)\($1.0)=\($1.1)&" }
+        let jsonData = jsonString.data(using: .utf8, allowLossyConversion: false)!
+        request.httpBody  = jsonData
 
         URLSession.shared.dataTask(with: request) { (data, response, error) in
             do{
                 let json = try JSON(data:data!)
-                print(json)
+                let url = json["data"]["url"]
+                
+                if(json["success"].boolValue == true){
+                    DispatchQueue.main.async{
+                                       self.image.kf.setImage(with: URL(string:url.stringValue))
+                                   }
+                }
+//               fixme:toast
             }catch(let catchedError){
                 print("error in parsing \(catchedError)")
             }
         }.resume()
-        
-//        print(myData)
-        image.kf.setImage(with: URL(string:myData!["url"].stringValue))
-        name.text = myData!["name"].stringValue
-//        Ici Texte 1
-        
-        
-//        Ici Texte 2
-        
-        
     }
-    
     
 }
 
